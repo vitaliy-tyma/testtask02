96e043b79f0bdf78bf40fb6a6528bf682f8097de
Tymashkov <VTymashkov@kiev.luxoft.com>
2019-02-25 18:30:41 +0200

    Initial commit

---
+++ src/main/java/info/sjd/HelloServlet.java
@@ -0,0 +1,12 @@
+package info.sjd;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>XXXXXXXXXXXXXXXXXXXXXX</h1
+<h2>Hello World!</h2>
+<h1>YYYYYYYYYYYYYYYYYYYYYY</h1
+</body>
+</html>
